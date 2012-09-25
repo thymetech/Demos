db2a7004f68dccbd1891ae4ecc93c0c20eb054f2
Mason Weems <mason@thymetech.com>
2012-09-25 12:28:31 -0500

    initial commit again

---
+++ MLWDemoTouchDB/MLWDemoTouchDB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MLWDemoTouchDB
+//
+//  Created by Mason Weems on 9/25/12.
+//  Copyright (c) 2012 Mason Weems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MLWDemoTouchDB/MLWDemoTouchDBTests/MLWDemoTouchDBTests.h
@@ -0,0 +1,13 @@
+//
+//  MLWDemoTouchDBTests.h
+//  MLWDemoTouchDBTests
+//
+//  Created by Mason Weems on 9/25/12.
+//  Copyright (c) 2012 Mason Weems. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MLWDemoTouchDBTests : SenTestCase
+
+@end
